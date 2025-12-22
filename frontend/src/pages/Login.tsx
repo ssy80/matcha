d28b2187ab40f78cd9ce66331649ce3efe710207
Axel Lee <axellee1994@gmail.com>
2025-12-22 23:52:03 +0800

    Completed Login Page and after logging in, points to Home (tempoary waypoint atm)

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,43 @@
-// This is a placeholder for a login page component
-
+import { useState } from 'react';
+import React from 'react';
+import api from '../api/axios';
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const navigate = useNavigate();
+    const handleSubmit = async (event: React.FormEvent) => {
+        event.preventDefault();
+
+        try {
+            const response = await api.post('/users/login', {
+                username,
+                password
+            });
+            localStorage.setItem('token', response.data.token)
+            console.log('Server says:', response.data);
+            alert('Login is successful!');
+            navigate('/home');
+        } catch (error: any) {
+            console.error('Error during login', error);
+            alert('Login failed:' + (error.response?.data?.error || error.message));
+        }
+    };
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>Username:</label>
+                <input type='text' value={username} onChange={e => setUsername(e.target.value)} />
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+            </div>
+            <button type='submit'>Login</button>
+        </form>
     );
 };
 
